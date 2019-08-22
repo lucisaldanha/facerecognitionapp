ddf2aaa8545f5a62ba86d40e51acc586e6f2ed1e
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-08-22 11:05:41 -0500

    deleted logo.svg and updated App.js with new components

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav style={{display:'flex', justifyContent:'flex-end'}} >
+			<p>Sign out</p>
+		</nav>
+	)
+}
+
+export default Navigation;
