83f0c856b4e4e751003882e331e0949b51dd1634
lusalga <ladsaldanha@gmail.com>
2019-08-27 18:06:33 -0500

    Created InputImageLink file and folder, adding input and button to it

--- src/components/InputImageLinkForm/InputImageLinkForm.js
+++ src/components/InputImageLinkForm/InputImageLinkForm.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 const InputImageLinkForm = () => {
 	return (
-		<div className=''>
-		
+		<div>
+			<p>
+				{'Please, paste a image link below. The Virtual Brain will detect faces within the image.'}
+			</p>
+			<div>
+				<input type='text' placeholder='drop image to see the magic!'/>
+				<button>Detect</button>
+			</div>
 		</div>
 	);  
 };
