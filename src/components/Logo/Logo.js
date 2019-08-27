eb472cf3c9a46716e507ebdd52f55861b3037d1f
lusalga <ladsaldanha@gmail.com>
2019-08-27 12:55:24 -0500

    added tachyons style on Tilt tag

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,8 +4,8 @@ import Tilt from 'react-tilt';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt" 
-				options={{ max : 55 }} 
+			<Tilt className="Tilt br2 shadow-2" 
+				options={{ max : 80 }} 
 				style={{ height: 120,
 					 width: 120, 
 					 display:'flex', 
