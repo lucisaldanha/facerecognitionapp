4524e5b0a578d5ed175f435870c6d498bc498f4f
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-08-27 17:02:35 -0500

    Created Logo.css, also added a new image to Logo.js, and styled it

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain1.png';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-2" 
 				options={{ max : 80 }} 
-				style={{ height: 120,
-					 width: 120, 
+				style={{ height: 150,
+					 width: 150, 
 					 display:'flex', 
 					 alignItems:'center', 
 					 justifyContent:'center'  
 				}} 
 			>
-				<div className="Tilt-inner"> 👽 </div>
+				<div className="Tilt-inner">
+					<img 
+					src={brain} alt='logo' 
+					/>
+				</div>
 			</Tilt>
 		</div>
-	);
+	);  
 };
 
 export default Logo;
