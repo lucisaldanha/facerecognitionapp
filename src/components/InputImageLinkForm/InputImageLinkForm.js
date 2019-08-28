aed160c9fa327a1436b95b280f0312961bf3462a
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-08-27 19:21:44 -0500

    Created a beautiful reusable flexbox div with input and button inside it.

--- src/components/InputImageLinkForm/InputImageLinkForm.js
+++ src/components/InputImageLinkForm/InputImageLinkForm.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './InputImageLinkForm.css'
 
 
 const InputImageLinkForm = () => {
 	return (
 		<div>
-			<p>
+			<p className='f4'>
 				{'Please, paste a image link below. The Virtual Brain will detect faces within the image.'}
 			</p>
-			<div>
-				<input type='text' placeholder='drop image to see the magic!'/>
-				<button>Detect</button>
+			<div className='centerFlex'>
+				<div className='centerFlex pa4 br3 shadow-3' >
+					<input className='w-70 pa2 f4 tc' type='text'  />
+					<button className='ml1 pointer br2 w-30 grow f4 link ph3 pv2 white bg-dark-green' >Detect</button>
+				</div>
 			</div>
 		</div>
 	);  
