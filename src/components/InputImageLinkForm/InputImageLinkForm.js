9dfdb061c246c3ce4a1cafaaab09e5809d6d3fbf
lusalga <ladsaldanha@gmail.com>
2019-08-29 17:14:56 -0500

    Adding Rank component and creating file and folder

--- src/components/InputImageLinkForm/InputImageLinkForm.js
+++ src/components/InputImageLinkForm/InputImageLinkForm.js
@@ -5,13 +5,15 @@ import './InputImageLinkForm.css'
 const InputImageLinkForm = () => {
 	return (
 		<div>
-			<p className='f4'>
-				{'Please, paste a image link below. The Virtual Brain will detect faces within the image.'}
+			<p className='f3 pt4'>
+				{'This Super Brain will detect faces in your pictures. Try it!'}
 			</p>
 			<div className='centerFlex'>
-				<div className='centerFlex pa4 br3 shadow-3' >
-					<input className='w-70 pa2 f4 tc' type='text'  />
-					<button className='ml1 pointer br2 w-30 grow f4 link ph3 pv2 white bg-dark-green' >Detect</button>
+				<div className='pattern centerFlex pa4 br3 shadow-3 mw ' >
+					<input className='w-70 pa2 f4 tc' type='text' placeholder='insert image link' />
+					<button className='ml1 pointer br2 w-30 grow f4 link ph3 pv2 dib white ' >
+						<span className="db w-100">Detect</span> 
+					</button>
 				</div>
 			</div>
 		</div>
