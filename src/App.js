fdf20ed55c0a2368efaf3ad79477a9c120c1523a
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-08-30 00:58:08 -0500

    Added Particles to App.js, but it needs improvements

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import InputImageLinkForm from './components/InputImageLinkForm/InputImageLinkForm';
 
-
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        }
+    }
+}
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Particles 
+                    className='particles'
+                    params={particlesOptions}
+                />
                 <Navigation />
                 <Logo />
                 <Rank />
