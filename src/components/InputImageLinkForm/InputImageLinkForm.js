fa7151dcd7dcb3bb26bd1f658053a4771adf6fdd
lusalga <ladsaldanha@gmail.com>
2019-09-02 17:20:51 -0500

    changed font colors

--- src/components/InputImageLinkForm/InputImageLinkForm.js
+++ src/components/InputImageLinkForm/InputImageLinkForm.js
@@ -10,8 +10,12 @@ const InputImageLinkForm = () => {
 			</p>
 			<div className='centerFlex'>
 				<div className='pattern centerFlex pa4 br3 shadow-3 mw ' >
-					<input className='w-70 pa2 f4 tc' type='text' placeholder='insert image link' />
-					<button className='ml1 pointer br2 w-30 grow f4 link ph3 pv2 dib white ' >
+					<input 
+						className='w-70 pa2 f4 tc' 
+						type='text' 
+						placeholder='insert image link' 
+					/>
+					<button className='ml1 pointer br2 w-30 grow f4 link ph3 pv2 dib gold ' >
 						<span className="db w-100">Detect</span> 
 					</button>
 				</div>
