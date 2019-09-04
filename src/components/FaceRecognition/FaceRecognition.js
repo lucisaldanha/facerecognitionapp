719c835c55bf7f8b206a81786aacb6a0cc60dd6b
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-09-04 16:13:13 -0500

    FaceRecognition component created, need to add props to it, and fixed issues with CSS

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+	return (
+		<div className = 'centerFlex' >
+			<div className = 'image' >
+				<img 
+					src = 'https://www.sciencefriday.com/wp-content/uploads/2019/06/D9bhA0iXsAAO-u7.jpg' 
+					alt = 'image link' 
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default FaceRecognition;
