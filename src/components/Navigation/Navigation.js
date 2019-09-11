533379bd25b3505531bbbb4026f173fa71f66d88
lusalga <ladsaldanha@gmail.com>
2019-09-11 14:57:27 -0500

    Implemented the routeChange function to modify the route state dynamically, when clicking to Sign In or Sign Out

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = ( {routeChange} ) => {
 	return (
 		<nav style={{display:'flex', justifyContent:'flex-end'}} >
-			<p 
+			<p
+				onClick={() => routeChange('home')}
 				className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
 				Sign out
 			</p>
