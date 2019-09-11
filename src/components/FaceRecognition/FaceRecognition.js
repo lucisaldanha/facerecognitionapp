ad14241120c5b00dc2ae40c9035647cc53a67bcd
lusalga <ladsaldanha@gmail.com>
2019-09-10 19:08:19 -0500

    Finished faceBox function, added box state to FaceRecognition.js. Changed CSS for FaceRecognition.

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ( {ImageUrlDisplay} ) => {
+const FaceRecognition = ( {ImageUrlDisplay, box} ) => {
 	return (
-		<div className = 'centerFlex' >
-			<div >
+		<div className = 'centerFlex' > {/* very important for css styling */ }
+			<div className=' absolute mt3'>{/* very important for css styling */ }
 				<img 
 					id = 'inputimage'
 					src = {ImageUrlDisplay}
 					alt = 'input entered' 
 					className = 'image'
 				/>
+				<div 
+					className='bounding-box' 
+					style = {{ top: box.topRow , right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}
+				>
+				</div>
 			</div>
 		</div>
 	)
