8deb62be6bf36d497fc0510680c69b47db42de34
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-09-12 18:01:44 -0500

    All button are working, all components are done. Ready to create a SERVER.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-const Navigation = ( {routeChange} ) => {
-	return (
-		<nav style={{display:'flex', justifyContent:'flex-end'}} >
-			<p
-				onClick={() => routeChange('signin')}
-				className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
-				Sign out
-			</p>
-		</nav>
-	)
+const Navigation = ( {routeChange, isSignedIn} ) => {
+	if ( isSignedIn ) {
+		return (
+			<nav style={{display:'flex', justifyContent:'flex-end'}} >
+				<p
+					onClick={() => routeChange('signin')}
+					className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
+					Sign out
+				</p>
+			</nav>
+		)
+	} else {
+		return (
+			<nav style={{display:'flex', justifyContent:'flex-end'}} >
+				<p
+					onClick={() => routeChange('signin')}
+					className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
+					Sign In
+				</p>
+				<p
+					onClick={() => routeChange('register')}
+					className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
+					Register
+				</p>
+			</nav>
+		)
+	}
 }
 
 export default Navigation;
+
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -8,15 +8,15 @@ const Register = ( {routeChange} ) => {
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 			      <legend className="f2 fw6 ph0 mh0 center">Register</legend>
 			      <div className="mt3">
-			        <label className="db fw6 lh-copy f6" for="name">Name</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
 			        <input className="br2 black pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
 			      </div>
 			      <div className="mt3">
-			        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 			        <input className="br2 black pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 			      </div>
 			      <div className="mv3">
-			        <label className="db fw6 lh-copy f6" for="password">Password</label>
+			        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 			        <input className="br2 black b pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 			      </div>
 			    </fieldset>
