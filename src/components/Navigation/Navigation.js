7bfe905136202bcd498044d2afa58ff562c4f84e
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2019-09-12 15:09:05 -0500

    Added new conditional statement to the previous one to test Register form. All event listeners working.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ( {routeChange} ) => {
 	return (
 		<nav style={{display:'flex', justifyContent:'flex-end'}} >
 			<p
-				onClick={() => routeChange('home')}
+				onClick={() => routeChange('signin')}
 				className='f4  ttc navy dim pa3 mv1 link underline-hover pointer'>
 				Sign out
 			</p>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ const SignIn = ( {routeChange} ) => {
 			    </fieldset>
 			    <div className="">
 			      <input 
-			      	onClick={() => routeChange('signin')} 
+			      	onClick={() => routeChange('home')} 
 			      	className="br2 black b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 			      	type="submit" 
 			      	value="Sign in" 
