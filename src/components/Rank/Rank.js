82009620c33f9c46c31937a1a9030158c7da1da3
lusalga <ladsaldanha@gmail.com>
2019-09-30 21:55:58 -0500

    Fixed user setState function in App.js

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ( {name, entries} ) => {
 	return (
 		<div>
 			<div className='f3 pa2 near-white ' >
-				{`${name}, your current rank is...`}
+				{`${name}, your current entry count is`}
 			</div>
 			<div className='f1 pa1 light-gray  ' >
 				{entries}
