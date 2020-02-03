66d0b6f046022004b7e1c8f7edb83c696f0db37a
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2020-02-03 11:51:04 -0600

    Register.js updated to fix Register same email issue.

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -41,7 +41,7 @@ class Register extends React.Component {
 		})
 			.then(response => response.json())
 			.then(user => {
-				if(user) {
+				if(user.id) {
 					this.props.loadUser(user);
 					this.props.routeChange('home');
 				} else {
